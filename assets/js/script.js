7448104bf604d9281ed58ae75b3b58b4b9e8b801
Michela <micheladellagatta1@gmail.com>
2025-10-23 14:24:59 +0200

    Add ul tag and get by id in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,15 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 const url = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const emails = [];
+const mailContainer = document.getElementById('mails');
 
 function getMails(n) {
   for (let i = 0; i<n; i++) {
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        emails.push(data.response)
+        const email = data.response;
+        emails.push(email);
       })
       .catch(error => console.error("Errore: ", error))
   }
