b4116d4d909c7c8730c930ededd6575dbb989e45
Michela <micheladellagatta1@gmail.com>
2025-10-23 14:11:45 +0200

    Fetch data from endpoint and check response in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
+
+const url = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => console.error("Errore: ", error))
