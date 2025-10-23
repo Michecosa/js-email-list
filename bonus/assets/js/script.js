80eaa7baf7d4cdc6688e2a310bd5460788b713f3
Michela <micheladellagatta1@gmail.com>
2025-10-23 14:28:00 +0200

    Create bonus folder

---
+++ bonus/assets/js/script.js
@@ -0,0 +1,28 @@
+/* 
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+const url = 'https://flynn.boolean.careers/exercises/api/random/mail';
+const emails = [];
+const mailContainer = document.getElementById('mails');
+
+function getMails(n) {
+  for (let i = 0; i<n; i++) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const email = data.response;
+        emails.push(email);
+
+        const li = document.createElement('li');
+        li.textContent = email;
+        mailContainer.appendChild(li);
+      })
+      .catch(error => console.error("Errore: ", error))
+  }
+}
+
+getMails(10);
+console.log(emails);
