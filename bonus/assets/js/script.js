44455aab15d374e2a90b82c9330b4aa27349487c
Michela <micheladellagatta1@gmail.com>
2025-10-23 16:52:07 +0200

    Add loading icon before showing all emails

--- bonus/assets/js/script.js
+++ bonus/assets/js/script.js
@@ -1,40 +1,43 @@
-/* 
-Abbellire con CSS o Bootstrap,
-Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
-*/
-
-const url = 'https://flynn.boolean.careers/exercises/api/random/mail';
+const url = "https://flynn.boolean.careers/exercises/api/random/mail";
 const emails = [];
-const mailContainer = document.getElementById('mails');
-const refresh = document.getElementById('refresh');
+const mailContainer = document.getElementById("mails");
+const refresh = document.getElementById("refresh");
 
 function getMails(n) {
-  mailContainer.innerHTML = "";
-  for (let i = 0; i<n; i++) {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const email = data.response;
-        emails.push(email);
+  emails.length = 0;
+  mailContainer.innerHTML = `
+    <div class="spinner-container text-center my-5">
+      <div class="spinner-border text-light" role="status" style="width: 3rem; height: 3rem;">
+        <span class="visually-hidden">Caricamento...</span>
+      </div>
+    </div>
+  `;
 
-        const elem = document.createElement('div');
-        elem.classList.add("col-sm-6", "mb-3")
-        elem.innerHTML = 
-        `
-        <div class="card rounded-5 h-100 mx-3">
-          <div class="card-body text-center">
-            <h5 class="card-title mb-0">${email}</h5>
-          </div>
-        </div>
-        `;
-        mailContainer.appendChild(elem);
-      }
-    )
-      .catch(error => console.error("Errore: ", error))
+  for (let i = 0; i < n; i++) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        emails.push(data.response);
+
+        if (emails.length === n) {
+          mailContainer.innerHTML = "";
+          emails.forEach((email) => {
+            const elem = document.createElement("div");
+            elem.classList.add("col-sm-6", "mb-3");
+            elem.innerHTML = `
+              <div class="card rounded-5 h-100 mx-3">
+                <div class="card-body text-center">
+                  <h5 class="card-title mb-0">${email}</h5>
+                </div>
+              </div>
+            `;
+            mailContainer.appendChild(elem);
+          });
+        }
+      })
+      .catch((error) => console.error("Errore: ", error));
   }
 }
 
 getMails(10);
-// console.log(emails);
-
-refresh.addEventListener('click', () => getMails(10))
+refresh.addEventListener("click", () => getMails(10));
